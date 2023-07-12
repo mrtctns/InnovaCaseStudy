023df98f066dbd2be0df2058fc541aacc4ba33df
Mert Çetin <mrtctns22@gmail.com>
2023-07-12 12:34:29 +0300

    Initial Commit

---
+++ InnovaCaseStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InnovaCaseStudy
+//
+//  Created by Mert Çetin on 12.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
